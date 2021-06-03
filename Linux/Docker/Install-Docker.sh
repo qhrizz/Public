3b2f4484f7ea5e87dbb95ad06907da918695652e
Christian Tamm <qhrizz@qhrizz.se>
2021-06-03 23:01:44 +0200

    Update Install-Docker.sh

--- Linux/Docker/Install-Docker.sh
+++ Linux/Docker/Install-Docker.sh
@@ -1,4 +1,5 @@
 #Tested on Ubuntu 18.04
+sudo apt update && apt upgrade -y
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -8,3 +9,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl enable docker && sudo systemctl start docker
